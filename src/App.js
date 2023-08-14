cfffa84618d12545975367b42ffc54867e6c8ef0
Madhu-9121 <madhusudhanc135@gmail.com>
2023-08-14 15:26:49 +0530

    card-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from './components/Herosection/Hero'
-
+import Albumcard from './components/Albums/Albumcard'
 function App() {
   return (
     <>
      <Navbar/>
      <Hero/>
+     <Albumcard img = {require("./assets/sample-img-card.png")} follows={123} category="New Bollywood" />
     </>
   );
 }
